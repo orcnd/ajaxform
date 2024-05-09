fc398d9468bcb8f399b0f3452b2d498582b05c98
orçun candan <orcuncandan89@gmail.com>
2024-05-09 10:52:16 +0300

    avatar upload fields and validation outputs

--- App/Views/login.blade.php
+++ App/Views/login.blade.php
@@ -3,19 +3,19 @@
     <div class="col col-md-4 col-sm-12">
         <form method="post" hx-post="?page=login" hx-target="#container">
             <h2>Login</h2>
-            <label class="mb-3">
+            <label class="mb-3 w-100">
                 Email
-                <input type="text" class="form-control
+                <input type="text" class="form-control 
                 {{(isset($validationErrors) && $validationErrors->first('email')!==null)?' is-invalid ':''}}" 
                 value="{{isset($validation)?$validation->getValidatedData()['email']:''}}"
                 name="email" />
                 @if((isset($validationErrors) && $validationErrors->first('email')!==null))
                 <div class="invalid-feedback">
-                    {{isset($validationErrors) && $validationErrors->first('email')}}
+                    {{isset($validationErrors) && $validationErrors->first('email')?$validationErrors->first('email'):''}}
                 </div>
                 @endif
             </label>
-            <label class="mb-3">
+            <label class="mb-3 w-100">
                 Password
                 <input type="password" class="form-control 
                 {{(isset($validationErrors) && $validationErrors->first('password')!==null)?' is-invalid ':''}}" 
@@ -23,14 +23,14 @@
                 name="password" />
                 @if((isset($validationErrors) && $validationErrors->first('password')!==null))
                 <div class="invalid-feedback">
-                    {{isset($validationErrors) && $validationErrors->first('password')}}
+                    {{isset($validationErrors) && $validationErrors->first('password')?$validationErrors->first('password'):''}}
                 </div>
                 @endif
             </label>
             @if (isset($loginFail))
-                <div class="invalid-feedback">
-                    Email or password is wrong
-                </div>
+            <div class="alert alert-danger" role="alert">
+                Email or password is wrong
+            </div>
             @endif 
             <input type="submit" value="Login" name="login" class="btn btn-primary w-100">
             <br><br><br><br>
--- App/Views/register.blade.php
+++ App/Views/register.blade.php
@@ -1,9 +1,11 @@
 
 <div class="row justify-content-md-center">
     <div class="col col-md-4 col-sm-12">
-        <form method="post" hx-post="?page=register" hx-target="#container">
+        <form method="post" id="form" hx-post="?page=register" hx-encoding="multipart/form-data" hx-target="#container"
+         _='on htmx:xhr:progress(loaded, total) set #progress.value to (loaded/total)*100'
+        >
             <h2>Register </h2>
-            <label class="mb-3">
+            <label class="mb-3 w-100">
                 Name
                 <input type="text" name="name" class="form-control 
                 {{(isset($validationErrors) && $validationErrors->first('name')!==null)?' is-invalid ':''}}" 
@@ -11,23 +13,35 @@
                 />
                 @if((isset($validationErrors) && $validationErrors->first('name')!==null))
                 <div class="invalid-feedback">
-                    {{isset($validationErrors) && $validationErrors->first('name')}}
+                    {{isset($validationErrors) && $validationErrors->first('name')?$validationErrors->first('name'):''}}
                 </div>
                 @endif
             </label>
-            <label class="mb-3">
+            <label class="mb-3 w-100 w-100">
                 Email
-                <input type="text" class="form-control
+                <input type="text" class="form-control w-100
                 {{(isset($validationErrors) && $validationErrors->first('email')!==null)?' is-invalid ':''}}" 
                 value="{{isset($validation)?$validation->getValidatedData()['email']:''}}"
                 name="email" />
                 @if((isset($validationErrors) && $validationErrors->first('email')!==null))
                 <div class="invalid-feedback">
-                    {{isset($validationErrors) && $validationErrors->first('email')}}
+                    {{isset($validationErrors) && $validationErrors->first('email')?$validationErrors->first('email'):''}}
                 </div>
                 @endif
             </label>
-            <label class="mb-3">
+            <label class="mb-3 w-100">
+                Avatar
+                <input class="form-control
+                {{(isset($validationErrors) && $validationErrors->first('avatar')!==null)?' is-invalid ':''}}" 
+                 type="file" name="avatar" />
+                <progress id='progress' value='0' max='100'></progress>
+                @if((isset($validationErrors) && $validationErrors->first('avatar')!==null))
+                <div class="invalid-feedback">
+                    {{isset($validationErrors) && $validationErrors->first('avatar')?$validationErrors->first('avatar'):''}}
+                </div>
+                @endif
+            </label>
+            <label class="mb-3 w-100">
                 Password
                 <input type="password" class="form-control 
                 {{(isset($validationErrors) && $validationErrors->first('password')!==null)?' is-invalid ':''}}" 
@@ -35,13 +49,13 @@
                 name="password" />
                 @if((isset($validationErrors) && $validationErrors->first('password')!==null))
                 <div class="invalid-feedback">
-                    {{isset($validationErrors) && $validationErrors->first('password')}}
+                    {{isset($validationErrors) && $validationErrors->first('password')?$validationErrors->first('password'):''}}
                 </div>
                 @endif
             </label>
-            <input type="submit" value="Register" name="register" class="btn btn-primary w-100">
+            <input type="submit" value="Register" name="register" class="btn btn-primary ">
             <br><br><br><br>
-            <button hx-target="#container" hx-get="?page=welcome" class="btn btn-secondary w-100"><< Back <<</button>
+            <button hx-target="#container" hx-get="?page=welcome" class="btn btn-secondary"><< Back <<</button>
         </form>
 
     </div>
